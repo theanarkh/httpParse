daf72256d4f8ace4cbad9f5409600b826b723242
yuebiaochen <2923878201@qq.com>
2017-07-20 01:55:39 +0800

    Update and rename test.txt to http.js

---
+++ http.js
@@ -0,0 +1,10 @@
+HTTP/1.1 200 OK
+Server: nginx
+Date: Wed, 05 Jul 2017 17:39:08 GMT
+Content-Type: text/html; charset=UTF-8
+Connection: close
+Vary: Accept-Encoding
+X-Powered-By: PHP/5.6.10
+Content-Length: 921
+
+xxxxxxxxxxxxxxxxxxxxxxxxxxxx
